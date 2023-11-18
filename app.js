d685969004d9aaa8ea8e11b494ab1eedf44237d6
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2023-11-18 08:05:29 +0100

    Basic express setup

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const http = require("http");
-const routes = require("./routes");
-const { log } = require("console");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
-console.log("it's working");
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
