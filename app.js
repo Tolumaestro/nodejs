8637d0af2054abfc1370ac4745cb21c87b4cd303
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2023-11-19 04:03:43 +0100

    implementing pug

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const rootDir = require("./util/path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "pug");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
