68a60e2e1e8634cf7609214db31e6053a3e25f12
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2023-11-20 15:15:11 +0100

    implementing ejs

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine("hbs", expressHbs({ defaultLayout: "main-layout", extname: "hbs" }));
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -19,7 +17,7 @@ app.use("/admin", adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: null });
 });
 
 app.listen(3000);
